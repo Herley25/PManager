13636971ed17b1f18a9de388a86e9201bdf4e40c
Herley Oliveira <herley.oliveira@bancoartico.com>
2025-07-12 22:35:41 -0300

    feat: create repository member

---
+++ src/main/java/com/java360/pmanager/domain/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.java360.pmanager.domain.repository;
+
+import com.java360.pmanager.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+    Optional<Member> findByIdAndDeleted(UUID id, boolean deleted);
+
+    Optional<Member> findByEmailAndDeleted(String email, boolean deleted);
+}
