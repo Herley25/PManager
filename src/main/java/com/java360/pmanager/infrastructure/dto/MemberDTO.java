c01149d4b7895b2e5b4e7219043d8243e28abf63
Herley Oliveira <herley.oliveira@bancoartico.com>
2025-07-27 22:08:43 -0300

    feat: add relationship in DTOs

--- src/main/java/com/java360/pmanager/infrastructure/dto/MemberDTO.java
+++ src/main/java/com/java360/pmanager/infrastructure/dto/MemberDTO.java
@@ -3,7 +3,12 @@ package com.java360.pmanager.infrastructure.dto;
 import com.java360.pmanager.domain.entity.Member;
 import lombok.Data;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toSet;
 
 @Data
 public class MemberDTO {
@@ -16,12 +21,19 @@ public class MemberDTO {
 
     private final String email;
 
+    private final Set<String> projectIds;
+
     public static MemberDTO create(Member member) {
         return new MemberDTO(
                 member.getId(),
                 member.getSecret(),
                 member.getName(),
-                member.getEmail()
+                member.getEmail(),
+                Optional.ofNullable(member.getProjects()) // Verifica se a lista de projetos é nula
+                        .orElse(List.of())
+                        .stream() // Verifica se a lista de projetos é nula e, se for, usa uma lista vazia
+                        .map(project -> project.getId().toString()) // Converter o UUID para String
+                        .collect(toSet()) // Coletar os IDs dos projetos em um Set de Strings
         );
     }
 }
